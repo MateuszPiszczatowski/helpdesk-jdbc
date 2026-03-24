d0a27b1fa2ff738a0784942c811e153cb3e7f036
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-24 20:43:16 +0100

    feat: add basic login to main menu

--- src/main/java/local/pk154938/shop/config/Main.java
+++ src/main/java/local/pk154938/shop/config/Main.java
@@ -1,9 +1,13 @@
 package local.pk154938.shop.config;
+import local.pk154938.shop.application.service.UserService;
+import local.pk154938.shop.application.session.Session;
 import local.pk154938.shop.ui.menu.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
-        MainMenu menu = new MainMenu();
+        Session session = new Session();
+        UserService userService = new UserService();
+        MainMenu menu = new MainMenu(userService, session);
         menu.show();
     }
 }
--- src/main/java/local/pk154938/shop/ui/menu/MainMenu.java
+++ src/main/java/local/pk154938/shop/ui/menu/MainMenu.java
@@ -1,7 +1,50 @@
 package local.pk154938.shop.ui.menu;
 
+import local.pk154938.shop.application.service.UserService;
+import local.pk154938.shop.application.session.Session;
+
 public class MainMenu {
-    public void show(){
-        System.out.println("Hello world");
+    private final UserService userService;
+    private final Session session;
+
+    public MainMenu(UserService userService, Session session){
+        this.userService = userService;
+        this.session = session;
     }
+
+    public void show(){
+        boolean running = true;
+
+        while(running){
+            System.out.println("=== MENU GŁÓWNE ===");
+            System.out.println("1. Zaloguj");
+            System.out.println("2. Wyjście");
+            System.out.print("Wybierz opcję: ");
+
+            String choice = System.console().readLine();
+
+            switch (choice){
+                case "1" -> handleLogin();
+                case "2" -> running = false;
+                default -> System.out.println("Niepoprawny wybór.");
+            }
+        }
+        System.out.println("Zamykanie programu...");
+    }
+
+    private void handleLogin(){
+        System.out.print("Login: ");
+        String login = System.console().readLine();
+        System.out.print("Hasło: ");
+        String password = System.console().readLine();
+
+        var user = userService.login(login, password);
+        if(user == null){
+            System.out.println("Błędne dane logowania");
+        }
+        else{
+            session.login(user);
+            System.out.println("Zalogowano jako: "+user.getUsername());
+        }
+    };
 }
