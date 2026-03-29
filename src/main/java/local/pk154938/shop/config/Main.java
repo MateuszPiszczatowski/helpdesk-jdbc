93a50772645031628d6884581869af152d0bc738
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-29 12:08:03 +0200

    feat: add UserRepository and mock admin to main

--- src/main/java/local/pk154938/shop/config/Main.java
+++ src/main/java/local/pk154938/shop/config/Main.java
@@ -1,12 +1,21 @@
 package local.pk154938.shop.config;
+import local.pk154938.shop.application.repository.UserRepository;
 import local.pk154938.shop.application.service.UserService;
 import local.pk154938.shop.application.session.Session;
+import local.pk154938.shop.domain.user.Role;
+import local.pk154938.shop.domain.user.User;
+import local.pk154938.shop.infrastructure.persistence.InMemoryUserRepository;
 import local.pk154938.shop.ui.menu.MainMenu;
+
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Session session = new Session();
-        UserService userService = new UserService();
+        UserRepository userRepository = new InMemoryUserRepository();
+        UserService userService = new UserService(userRepository);
+        User user = new User("Admin", "Admin", Set.of(Role.ADMIN));
+        userRepository.save(user);
         MainMenu menu = new MainMenu(userService, session);
         menu.show();
     }
