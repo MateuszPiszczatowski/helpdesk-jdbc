1b8f65a25b2c34bbdaacf526e6f9e7d01c26d8dd
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-03-29 11:09:34 +0200

    feat: add user repository interface

---
+++ src/main/java/local/pk154938/shop/application/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package local.pk154938.shop.application.repository;
+
+import local.pk154938.shop.domain.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    void save(User user);
+    Optional<User> findByUsername(String username);
+    List<User> findAll();
+    void delete(String username);
+}
