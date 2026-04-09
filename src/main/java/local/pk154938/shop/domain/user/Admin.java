987067deca45477d4f4ef080a7acad2628bb8c38
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-04-09 14:08:29 +0200

    feat: add UUID to User and User copying mechanism

--- src/main/java/local/pk154938/shop/domain/user/Admin.java
+++ src/main/java/local/pk154938/shop/domain/user/Admin.java
@@ -1,9 +1,18 @@
 package local.pk154938.shop.domain.user;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class Admin extends User{
     public Admin(String username, String hashedPassword, String salt, Set<Role> roles){
         super(username, hashedPassword, salt, roles);
+    }
+    private Admin(UUID id, String username, String hashedPassword, String salt, Set<Role> roles){
+        super(id, username, hashedPassword, salt, roles);
+    }
+
+    @Override
+    protected User copy(String username, String password) {
+        return new Admin(this.getId(), username, password, this.getSalt(), this.getRoles());
     }
 }
--- src/main/java/local/pk154938/shop/domain/user/Manager.java
+++ src/main/java/local/pk154938/shop/domain/user/Manager.java
@@ -1,10 +1,19 @@
 package local.pk154938.shop.domain.user;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class Manager extends Employee{
     public Manager(String username, String hashedPassword, String salt, Set<Role> roles)
     {
         super(username, hashedPassword, salt, roles);
+    }
+    private Manager(UUID id, String username, String hashedPassword, String salt, Set<Role> roles){
+        super(id, username, hashedPassword, salt, roles);
+    }
+
+    @Override
+    protected User copy(String username, String password) {
+        return new Manager(this.getId(), username, password, this.getSalt(), this.getRoles());
     }
 }
